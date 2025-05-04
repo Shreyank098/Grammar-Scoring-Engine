fdf18ebe4e9d7cf653788fad144f7713f336313f
Shreyank Kiran <shreyank098@gmail.com>
2025-05-05 01:52:09 +0530

    Update feature_extraction.py

--- Grammar-Scoring-Engine/src/feature_extraction.py
+++ Grammar-Scoring-Engine/src/feature_extraction.py
@@ -1,1 +1,21 @@
+import numpy as np
+import librosa
+
+def extract_features(file_path):
+    audio, sr = librosa.load(file_path, sr=None)
+
+    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=13)
+    chroma = librosa.feature.chroma_stft(y=audio, sr=sr)
+    spectral_contrast = librosa.feature.spectral_contrast(y=audio, sr=sr)
+    zero_crossing = librosa.feature.zero_crossing_rate(y=audio)
+    rmse = librosa.feature.rms(y=audio)
+
+    features = np.hstack([
+        np.mean(mfcc, axis=1),
+        np.mean(chroma, axis=1),
+        np.mean(spectral_contrast, axis=1),
+        np.mean(zero_crossing),
+        np.mean(rmse)
+    ])
+    return features
 
